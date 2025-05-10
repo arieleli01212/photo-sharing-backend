5405258485fd1a8bf1bee39f7ea7777eec93a4c3
barak <barak.panker@gmail.com>
2025-05-10 15:09:01 +0300

    add a guest counter

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException, WebSocket, WebSocketDisconnect
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import os
 import shutil
+from typing import List
 
 app = FastAPI(
     title="Image Upload API",
@@ -17,11 +18,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
-
+guest_count = 0
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-IP = "172.20.10.6"
+active_connections: List[WebSocket] = []
+IP = "127.0.0.1"
 PORT = 8000
 app.mount("/uploads", StaticFiles(directory=UPLOAD_DIR), name="uploads")
 
@@ -58,3 +60,35 @@ async def get_images():
         return image_files
     except Exception as e:
         return {"error": str(e)}
+
+
+@app.get("/guest")
+async def get_guest_count():
+    return {"count": guest_count}
+
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    global guest_count
+    await websocket.accept()
+    active_connections.append(websocket)
+    guest_count += 1
+
+
+    await broadcast_guest_count()
+
+    print("➕ connected", guest_count)
+
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        active_connections.remove(websocket)
+        guest_count = max(guest_count - 1, 0)
+        await broadcast_guest_count()
+        print("➖ disconnected", guest_count)
+
+async def broadcast_guest_count():
+    for connection in active_connections:
+        await connection.send_json({"guestCount": guest_count})
