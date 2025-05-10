9f1c467a38c55245b711b30044502d4ec77ca920
barak <barak.panker@gmail.com>
2025-05-10 17:40:29 +0300

    connect to DB

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from database import Base
+
+class Image(Base):
+    __tablename__ = "images"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, nullable=False)
+    filepath = Column(String, nullable=False)
+    uploaded_at = Column(DateTime, default=datetime.utcnow)
